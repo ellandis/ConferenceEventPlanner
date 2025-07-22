bb92fdbc5a3c208662f869965fb1e9fd95591a93
Eian Landis <elamarlandis@gmail.com>
2025-07-22 12:49:38 -0400

    setup repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LandingPange from './components/LandingPage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <LandingPange/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,24 @@
+const LandingPage = () => {
+  return (
+    <>
+      <div className="lpFormat">
+        <p>Conference_Expense Landing page</p>
+        <p>Plan your next major event with us!</p>
+        <button>Get Started</button>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
