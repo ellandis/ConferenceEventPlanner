b13ab9eabf11e35338abd95c3baaee622f11b7a1
Eian Landis <elamarlandis@gmail.com>
2025-07-27 11:40:35 -0400

    added card product pics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import LandingPage from "./components/LandingPage.jsx";
 import "./App.css";
 import ProductPage from "./components/ProductPage.jsx";
-import Nav  from "./components/Nav.jsx";
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <>
-          <LandingPage/>
-          <ProductPage/>
+      <LandingPage />
+      <ProductPage />
     </>
   );
 }
