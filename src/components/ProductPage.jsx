1155c1ae9a09d59d926f6ae112f324847e712c4e
ellandis <elamarlandis@gmail.com>
2025-08-05 11:14:41 -0400

    imported pics

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -4,6 +4,15 @@ import ConfImg from '../assets/conference.jpeg'
 import PresImg from '../assets/presentation.jpeg'
 import LargeImg from '../assets/large.jpeg'
 import SmallImg from '../assets/small.jpeg'
+import  SpeakersImg from '../assets/speaker.jpg'
+import MicrophonesImg from '../assets/micophone.jpg'
+import WhiteboardsImg from '../assets/whiteboard.jpeg'
+import ProjectorsImg from '../assets/projector.jpg'
+import SignageImg from '../assets/sinage.jpeg'
+import BreakfastImg from '../assets/Lumberjack.jpg'
+import LunchImg from '../assets/lunch.jpg'
+import HighTeaImg from '../assets/hightea.jpg'
+import DinnerImg from '../assets/dinner.jpg'
 import { useDispatch, useSelector } from "react-redux";
 import {addItemToCart} from './CartSlice';
 
@@ -21,16 +30,23 @@ const ProductPage = () => {
   { id: 3, name: "Presentation room", price: 700, img: PresImg },
   { id: 4, name: "Large room", price: 900, img: LargeImg },
   { id: 5, name: "Small room", price: 1100, img: SmallImg },
-  { id: 6, name: "Speakers", price: 35, img: SpeakersImg },
+  
+];
+
+const addons =[
+{ id: 6, name: "Speakers", price: 35, img: SpeakersImg },
   { id: 7, name: "Microphones", price: 45, img: MicrophonesImg },
   { id: 8, name: "Whiteboards", price: 80, img: WhiteboardsImg },
   { id: 9, name: "Projectors", price: 200, img: ProjectorsImg },
   { id: 10, name: "Signage", price: 80, img: SignageImg },
+]
+
+const meals = [
   { id: 11, name: "Breakfast", price: 50, img: BreakfastImg },
   { id: 12, name: "Lunch", price: 60, img: LunchImg },
   { id: 13, name: "High tea", price: 25, img: HighTeaImg },
   { id: 14, name: "Dinner", price: 70, img: DinnerImg },
-];
+]
 
 
 
